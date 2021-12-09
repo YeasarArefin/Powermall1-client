b85e8d4d6c457c3e56f68c98217ab6d6ee8b69cf
S.M.ABTAHI NOOR <abtahinoorkabid@gmail.com>
2021-12-09 18:23:48 +0600

    Sign Up Page UI Done

--- src/components/Sign Up/SignUpForm.js
+++ src/components/Sign Up/SignUpForm.js
@@ -1,47 +1,50 @@
-import React from 'react';
-import { FcGoogle } from 'react-icons/fc';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { FcGoogle } from 'react-icons/fc'
+import { Link } from 'react-router-dom'
 
-const LoginForm = () => {
+const SignUpForm = () => {
     return (
         <div className='my-6 bg-white p-4 py-8 box-border rounded-lg'>
             <form action="">
                 <div className="flex flex-col space-y-4">
+                    <div className="flex flex-col space-y-2">
+                        <label htmlFor="name" className='text-gray-600 text-base'>Name</label>
+                        <input className="input" type="text" name="input" id="name" placeholder='John Doe' />
+                    </div>
                     <div className="flex flex-col space-y-2">
                         <label htmlFor="email" className='text-gray-600 text-base'>Email</label>
                         <input className="input" type="email" name="input" id="email" placeholder='example@gmail.com' />
                     </div>
                     <div className="flex flex-col space-y-2">
                         <label htmlFor="password" className='text-gray-600 text-base flex justify-between items-center'>
-                            <span>Password</span>
-                            <span className='cursor-pointer text-sm hover:underline text-primary'>Forgot Password ?</span>
+                            <span>Password (At least 6 characters)</span>
                         </label>
                         <input type="password" name="input" id="password" className="input" placeholder='********' />
                     </div>
 
-                    {/* Login button  */}
+                    {/* Sign button  */}
                     <div className='py-4'>
-                        <button type="submit" className='py-3 px-4 w-full bg-primary text-white rounded-md hover:bg-blue-600 transition duration-300'>Login</button>
+                        <button type="submit" className='py-3 px-4 w-full bg-primary text-white rounded-md hover:bg-blue-600 transition duration-300'>Sign In</button>
                         <p className='text-gray-600 text-center py-4'>Or</p>
-                    </div> 
+                    </div>
                 </div>
             </form>
 
-            
+
             <div>
                 {/* google button  */}
                 <button type="submit" className='py-3 px-4 w-full text-gray-600 flex items-center justify-center space-x-3 rounded-md border'>
                     <FcGoogle className='text-2xl' />
-                    <span>Login with Google</span>
+                    <span>Sign In with Google</span>
                 </button>
 
                 {/* register  */}
                 <div className='mt-6 pt-4 border-t border-gray-200'>
-                    <p className='text-center text-gray-600'>Don't have any account ? <Link to="/signup" className='text-primary font-bold underline cursor-pointer'>Signup</Link> </p>
+                    <p className='text-center text-gray-600'>Already have any account ? <Link to="/login" className='text-primary font-bold underline cursor-pointer'>Login</Link> </p>
                 </div>
             </div>
         </div>
     )
 }
 
-export default LoginForm
+export default SignUpForm
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import SignUpForm from '../components/Sign Up/SignUpForm'
 
 const SignUp = () => {
     return (
-        <div>
-            <h1>SignUp</h1>
-        </div>
+        <main className="bg-blue-50 h-screen w-full">
+            <div className="max-w-screen-xl mx-auto px-6 flex flex-col h-screen lg:justify-center justify-start">
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 place-content-center">
+                    {/* vector image  */}
+                    <div className="hidden lg:block order-2">
+                        <img src="../../assets/signUp.png" alt="login" className=" object-contain mx-auto" />
+                    </div>
+
+                    {/* sign up form  */}
+                    <div className="flex flex-col items-center order-1">
+                        {/* brand  */}
+                        <div className="flex flex-col items-center space-y-4">
+                            <img src="../../../assets/logo.png" alt="logo" className="object-contain w-36 cursor-pointer" />
+                            <p className="text-center text-gray-600">Sign Up with your email & password</p>
+                        </div>
+
+                        {/* signup form  */}
+                        <SignUpForm />
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
 
