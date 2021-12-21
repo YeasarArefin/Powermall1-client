a98c20d7269e577fca04dbe886c6f671539887ca
S.M.ABTAHI NOOR <abtahinoorkabid@gmail.com>
2021-12-21 23:53:52 +0600

    Shops page init done

--- src/components/Popular Categories/PopularCategories.js
+++ src/components/Popular Categories/PopularCategories.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Slide from 'react-reveal/Slide';
 import SectionTitle from '../Section Title/SectionTitle';
 
 const PopularCategories = () => {
-    const categories = [
-        { id: 1, name: 'Mobile', image: '../../../assets/categories/smartphone.png' },
-        { id: 1, name: 'Laptop', image: '../../../assets/categories/laptop.png' },
-        { id: 1, name: 'Headphone', image: '../../../assets/categories/headphones.png' },
-        { id: 1, name: 'Camera', image: '../../../assets/categories/camera.png' },
-        { id: 1, name: 'AC', image: '../../../assets/categories/ac.png' },
-        { id: 1, name: 'Keyboard', image: '../../../assets/categories/keyboard.png' },
-        { id: 1, name: 'Printer', image: '../../../assets/categories/printer.png' },
-        { id: 1, name: 'Pendrive', image: '../../../assets/categories/pendrive.png' },
-    ]
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        axios.get('https://electro-comers-server.herokuapp.com/category')
+            .then(res => setCategories(res.data))
+    }, [])
+
 
     return (
         <>
@@ -22,10 +20,10 @@ const PopularCategories = () => {
                 {/* categories  */}
                 <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-10 py-8">
                     {categories?.map(item => (
-                        <Slide bottom key={item.id}>
+                        <Slide bottom key={item._id}>
                             <div className='bg-white rounded-lg p-3 box-border shadow-xl flex flex-col items-center justify-center h-full space-y-2 hover:scale-105 transform transition duration-500 cursor-pointer'>
-                                <img className="w-16 object-contain" src={item.image} alt={item.name} />
-                                <p className="text-gray-600">{item.name}</p>
+                                <img className="w-16 object-contain" src={item.img} alt={item.category} />
+                                <p className="text-gray-600">{item.category}</p>
                             </div>
                         </Slide>
                     ))}
--- src/pages/Shops.js
+++ src/pages/Shops.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import Footer from '../components/Footer/Footer'
+import React from 'react';
+import Footer from '../components/Footer/Footer';
+import ProductCard from '../components/Products/ProductCard';
+import useFetch from '../hooks/useFetch';
 
 const Shops = () => {
+    const [products] = useFetch();
+
+
     return (
         <>
-            <main className="max-w-screen-xl mx-auto px-6" style={{ background: '#F4F4FA', height: '100%' }}>
+            <main className="max-w-screen-xl mx-auto px-6 py-12" style={{ background: '#F4F4FA', height: '100%' }}>
                 {/* title  */}
-                <h1>A</h1>
+                <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10'>
+                    {products?.map(product => (
+                        <ProductCard key={product.id} {...product} />
+                    ))}
+                </div>
             </main>
             <Footer />
         </>
