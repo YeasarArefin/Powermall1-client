9a018b09a5bbd6a1173f359b78f7d4221132a7e5
S.M.ABTAHI NOOR <abtahinoorkabid@gmail.com>
2021-12-22 18:46:33 +0600

    Header banner Fetch Done

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import "swiper/components/effect-coverflow/effect-coverflow.min.css";
 import "swiper/components/pagination/pagination.min.css";
 import SwiperCore, { Autoplay, EffectCoverflow, Pagination } from 'swiper/core';
@@ -7,20 +8,22 @@ import "swiper/swiper.min.css";
 SwiperCore.use([EffectCoverflow, Pagination, Autoplay]);
 
 const Slider = () => {
-    const images = [
-        { id: 1, image: 'https://links.papareact.com/gi1'}, 
-        { id: 1, image: 'https://links.papareact.com/6ff'},
-        { id: 1, image: 'https://links.papareact.com/7ma'},
-    ]
+    const [image,setImage] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://electro-comers-server.herokuapp.com/headerbanner')
+        .then(res => setImage(res?.data?.[0]?.img?.split(',')))
+    }, [image])
+
     return (
         <section id="hero">
             <Swiper pagination={true} autoplay={{
                 delay: 2000,
                 disableOnInteraction: false
             }} speed={150} className="mySwiper">
-                {images?.map(image => (
-                    <SwiperSlide key={image.id}>
-                        <img className="object-cover w-full" src={image.image} alt="slider" />
+                {image?.map((item,index) => (
+                    <SwiperSlide key={index}>
+                        <img className="object-cover w-full" src={item} alt="slider" />
                     </SwiperSlide>
                 ))}
             </Swiper>
