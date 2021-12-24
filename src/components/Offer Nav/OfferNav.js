d39672c9332bdfb7ce20eadab4f7541b58d67b62
S.M.ABTAHI NOOR <abtahinoorkabid@gmail.com>
2021-12-24 23:38:11 +0600

    notification work done

--- src/components/Offer Nav/OfferNav.js
+++ src/components/Offer Nav/OfferNav.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { MdClose } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
-const OfferNav = ({ setShowOffer}) => {
-    return (
-        <div className="bg-primary text-white py-2">
-            <div className="max-w-screen-xl mx-auto px-6 flex items-center flex-grow">
-                {/* text  */}
-                <div className='flex items-center space-x-2 flex-grow justify-center'>
-                    <p>Hurry! Order by Dec. 24th, 1pm & choose Express Delivery to get gifts & more in 2 hours or less</p>
-                    <Link to="/shops"> <ins>Shop Now</ins></Link>
-                </div>
+const OfferNav = ({ setShowOffer }) => {
+    const [notification, setNotification] = useState({})
 
-                {/* close  */}
-                <div>
-                    <MdClose className='cursor-pointer' onClick={() => setShowOffer(false)} />
+    useEffect(() => {
+        axios.get("https://electro-comers-server.herokuapp.com/notification/61c4ec647808158ad961db48")
+            .then(res => setNotification(res?.data))
+    }, [notification?.status])
+
+    return (
+        notification?.status?.toLowerCase() === "on" && (
+            <>
+                <div className="bg-primary text-white py-2">
+                    <div className="max-w-screen-xl mx-auto px-6 flex items-center flex-grow">
+                        {/* text  */}
+                        <div className='flex items-center space-x-2 flex-grow justify-center'>
+                            <p>{notification?.message}</p>
+                            <Link to="/shops"> <ins>Shop Now</ins></Link>
+                        </div>
+
+                        {/* close  */}
+                        <div>
+                            <MdClose className='cursor-pointer' onClick={() => setShowOffer(false)} />
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </>
+        )
     )
 }
 
