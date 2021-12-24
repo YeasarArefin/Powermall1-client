6efdd93e3aeec282ca24576a5aea6dfbd2af7433
S.M.ABTAHI NOOR <abtahinoorkabid@gmail.com>
2021-12-25 00:47:49 +0600

    Set Global Indicator Done

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
+import FadeLoader from "react-spinners/FadeLoader";
 import useAuth from '../hooks/useAuth';
 
 const PrivateRoute = ({ children }) => {
     const { user, isLoading } = useAuth();
     const location = useLocation();
 
-    if (isLoading) { return <h1>Loading....</h1>}
+    if (isLoading) { return (
+        <div className='flex flex-col h-screen w-full justify-center items-center space-y-6'>
+            <FadeLoader color="#11A0DB" loading={isLoading} size={50} />
+        </div>
+    ) }
 
     return user?.email ? children : <Navigate to="/login" state={{ from: location }} />;
 
