d6d70a312d5182933e2ed0d7e263cfef943cf52d
S.M.ABTAHI NOOR <abtahinoorkabid@gmail.com>
2022-01-07 01:53:53 +0600

    firebase api changed

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,18 +1,18 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
-    // apiKey: "AIzaSyCa48CSnWyurO_rlkjpFGoDfsP-TjWwfzk",
-    // authDomain: "ema-john-9ca17.firebaseapp.com",
-    // projectId: "ema-john-9ca17",
-    // storageBucket: "ema-john-9ca17.appspot.com",
-    // messagingSenderId: "222981343989",
-    // appId: "1:222981343989:web:9be1b7d6be2e5db8a013bc"
+    // apiKey: process.env.REACT_APP_API_KEY,
+    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    // projectId: process.env.REACT_APP_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    // appId: process.env.REACT_APP_APP_ID
+    apiKey: "AIzaSyAa7qiYFg_XzRMX0f_CgtpsJM83ToXJUPs",
+    authDomain: "e-shop-b7ee1.firebaseapp.com",
+    projectId: "e-shop-b7ee1",
+    storageBucket: "e-shop-b7ee1.appspot.com",
+    messagingSenderId: "739244304640",
+    appId: "1:739244304640:web:a29b17fccdbb01c246ebe5"
 };
 
 // Initialize Firebase
