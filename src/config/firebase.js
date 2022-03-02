eae980dd674398517ccaaa13f7dd1a0bd366722c
19smabtahinoor <abtahinoorkabid@gmail.com>
2022-03-02 23:39:15 +0600

    firebase updated

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,12 +7,12 @@ const firebaseConfig = {
     // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     // appId: process.env.REACT_APP_APP_ID
-    apiKey: "AIzaSyAa7qiYFg_XzRMX0f_CgtpsJM83ToXJUPs",
-    authDomain: "e-shop-b7ee1.firebaseapp.com",
-    projectId: "e-shop-b7ee1",
-    storageBucket: "e-shop-b7ee1.appspot.com",
-    messagingSenderId: "739244304640",
-    appId: "1:739244304640:web:a29b17fccdbb01c246ebe5"
+    apiKey: "AIzaSyDwZhp1DFWfIfs40AWGxhy3nPu1jjvPh5Q",
+    authDomain: "powermall-1291a.firebaseapp.com",
+    projectId: "powermall-1291a",
+    storageBucket: "powermall-1291a.appspot.com",
+    messagingSenderId: "125417358657",
+    appId: "1:125417358657:web:cb98684c89784200c44dc0"
 };
 
 // Initialize Firebase
