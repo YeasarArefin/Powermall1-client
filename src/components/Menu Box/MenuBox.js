273105da0420a9e5af4290f9eb542bfec3d80377
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-03-21 16:10:24 +0600

    single purchase updated

--- src/components/Menu Box/MenuBox.js
+++ src/components/Menu Box/MenuBox.js
@@ -1,35 +1,66 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const MenuBox = () => {
-    const menus = [
-        { id: 1, name: 'Single Puchase', link: '/singlepurchase', img: "../../../assets/icons/5.jpg" },
-        { id: 10, name: 'Free Shipping', link: '/free', img: "../../../assets/icons/2.jpg" },
-        { id: 3, name: 'Global Purchase', link: '/globalpurchase', img: "../../../assets/icons/3.jpg" },
-        { id: 2, name: 'Drone Service', link: '/droneservice', img: "../../../assets/icons/1.jpg" },
-        { id: 4, name: 'Mobile', link: '/mobile', img: "../../../assets/icons/4.jpg" },
-    ]
+  const menus = [
+    {
+      id: 1,
+      name: "Single Purchase",
+      link: "/singlepurchase",
+      img: "../../../assets/icons/5.jpg",
+    },
+    {
+      id: 10,
+      name: "Free Shipping",
+      link: "/free",
+      img: "../../../assets/icons/2.jpg",
+    },
+    {
+      id: 3,
+      name: "Global Purchase",
+      link: "/globalpurchase",
+      img: "../../../assets/icons/3.jpg",
+    },
+    {
+      id: 2,
+      name: "Drone Service",
+      link: "/droneservice",
+      img: "../../../assets/icons/1.jpg",
+    },
+    {
+      id: 4,
+      name: "Mobile",
+      link: "/mobile",
+      img: "../../../assets/icons/4.jpg",
+    },
+  ];
 
-   
+  return (
+    <div className="mt-3">
+      <h1 className="text-gray-700 text-left py-4 capitalize text-xl font-semibold ">
+        Special Categories
+      </h1>
+      <div className="grid grid-cols-5 md:grid-cols-5 lg:grid-cols-5 place-content-center gap-x-3 gap-y-3 md:gap-y-3 lg:gap-x-4">
+        {menus?.map((item) => (
+          <Link key={item?.id} to={item?.link}>
+            <div className="flex flex-col  lg:flex-row items-center lg:space-x-2  bg-white rounded-md space-y-2 lg:space-y-0 h-28  lg:h-auto justify-start lg:justify-start overflow-hidden px-2 md:px-0 lg:px-0">
+              <div className="bg-none md:bg-none h-14  flex flex-col justify-center items-center">
+                <img
+                  className="w-14 object-contain"
+                  src={item?.img}
+                  alt={item?.name}
+                />
+              </div>
 
-    return (
-        <div className='mt-3'>
-            <h1 className="text-gray-700 text-left py-4 capitalize text-xl font-semibold ">Special Categories</h1>
-            <div className='grid grid-cols-5 md:grid-cols-5 lg:grid-cols-5 place-content-center gap-x-3 gap-y-3 md:gap-y-3 lg:gap-x-4'>
-                {menus?.map(item => (
-                    <Link key={item?.id} to={item?.link}>
-                        <div className='flex flex-col  lg:flex-row items-center lg:space-x-2  bg-white rounded-md space-y-2 lg:space-y-0 h-28  lg:h-auto justify-start lg:justify-start overflow-hidden px-2 md:px-0 lg:px-0'>
-                            <div className='bg-none md:bg-none h-14  flex flex-col justify-center items-center'>
-                                <img className="w-14 object-contain" src={item?.img} alt={item?.name} />
-                            </div>
-
-                            <p className='text-gray-700 text-center  lg:text-left categoryTitle' >{item?.name}</p>
-                        </div>
-                    </Link>
-                ))}
+              <p className="text-gray-700 text-center  lg:text-left categoryTitle">
+                {item?.name}
+              </p>
             </div>
-        </div>
-    );
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MenuBox;
--- src/components/Popular Categories/PopularCategories.js
+++ src/components/Popular Categories/PopularCategories.js
@@ -1,93 +1,110 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Link, useSearchParams } from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 import "swiper/components/effect-coverflow/effect-coverflow.min.css";
 import "swiper/components/navigation/navigation.min.css";
 import "swiper/components/pagination/pagination.min.css";
-import SwiperCore, { Autoplay, EffectCoverflow, Navigation, Pagination } from 'swiper/core';
+import SwiperCore, {
+  Autoplay,
+  EffectCoverflow,
+  Navigation,
+  Pagination,
+} from "swiper/core";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.min.css";
 SwiperCore.use([EffectCoverflow, Pagination, Autoplay, Navigation]);
 
 const PopularCategories = () => {
-    const [categories, setCategories] = useState([])
-        // eslint-disable-next-line no-unused-vars
-    const [searchParams, setSearchParams] = useSearchParams();
+  const [categories, setCategories] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [searchParams, setSearchParams] = useSearchParams();
 
-    useEffect(() => {
-        axios.get('https://api.powermall.com.bd/category')
-            .then(res => {
-                const newData = res?.data?.sort((a, b) => a?.position - b?.position);
-                setCategories(newData)
-            })
-    }, [])
+  useEffect(() => {
+    axios.get("https://api.powermall.com.bd/category").then((res) => {
+      const newData = res?.data?.sort((a, b) => a?.position - b?.position);
+      setCategories(newData);
+    });
+  }, []);
 
+  return (
+    <>
+      <section className="py-6">
+        <h1 className="text-gray-700 text-left py-3 capitalize text-xl font-semibold">
+          Popular Categories
+        </h1>
+        <Swiper
+          loop={false}
+          navigation={true}
+          slidesPerView={8}
+          centeredSlides={false}
+          spaceBetween={10}
+          grabCursor={true}
+          pagination={{
+            clickable: true,
+          }}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          className="mySwiper py-8 pb-10 px-10"
+          breakpoints={{
+            320: {
+              slidesPerView: 3,
+            },
+            375: {
+              slidesPerView: 3,
+            },
 
-    return (
-        <>
-            <section className="py-6">
-                <h1 className="text-gray-700 text-left py-3 capitalize text-xl font-semibold">Popular Categories</h1>
-                <Swiper
-                    loop={false}
-                    navigation={true}
-                    slidesPerView={8}
-                    centeredSlides={false}
-                    spaceBetween={10}
-                    grabCursor={true}
-                    pagination={{
-                        "clickable": true
-                    }}
-                    autoplay={{
-                        delay: 3000,
-                        disableOnInteraction: false
-                    }}
-                    className="mySwiper py-8 pb-10 px-10" breakpoints={{
-                        320: {
-                            slidesPerView: 3,
-                        },
-                        375: {
-                            slidesPerView:3,
-                        },
-                        
-                        414: {
-                            slidesPerView: 3,
-                        },
-                        
-                        425: {
-                            slidesPerView: 3,
-                        },
-                        500: {
-                            slidesPerView: 4,
-                        },
-                        768: {
-                            slidesPerView: 6,
-                        },
-                        1024: {
-                            slidesPerView: 6
-                        },
-                        1200: {
-                            slidesPerView: 8
-                        },
-                        1400: {
-                            slidesPerView: 8
-                        }
-                    }}>
-                    {categories?.map(item => (
-                        <SwiperSlide key={item._id} className="h-full">
-                                <Link to={`/shops?categories=${item?.slug}`} onClick={() => {
-                                    setSearchParams({ categories: item?.slug })
-                                }}>
-                                    <div className='bg-white rounded-md p-4 box-border flex flex-col items-center justify-center h-full space-y-2 hover:scale-105 transform transition duration-500 cursor-pointer' style={{height:'120px'}}>
-                                        <img className="w-12 md:w-14 lg:w-w-14 object-contain" src={item.img} alt={item.category} />
-                                        <p className="text-gray-600 text-xs pt-2">{item.category}</p>
-                                    </div>
-                                    </Link>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </section>
-        </>
-    )
-}
+            414: {
+              slidesPerView: 3,
+            },
 
-export default PopularCategories
+            425: {
+              slidesPerView: 3,
+            },
+            500: {
+              slidesPerView: 4,
+            },
+            768: {
+              slidesPerView: 6,
+            },
+            1024: {
+              slidesPerView: 6,
+            },
+            1200: {
+              slidesPerView: 8,
+            },
+            1400: {
+              slidesPerView: 8,
+            },
+          }}
+        >
+          {categories?.map((item) => (
+            <SwiperSlide key={item._id} className="h-full">
+              <Link
+                to={`/shops?categories=${item?.slug}`}
+                onClick={() => {
+                  setSearchParams({ categories: item?.slug });
+                }}
+              >
+                <div
+                  className="bg-white rounded-md p-4 box-border flex flex-col items-center justify-center h-full space-y-2 hover:scale-105 transform transition duration-500 cursor-pointer"
+                  style={{ height: "120px" }}
+                >
+                  <img
+                    className="w-12 md:w-14 lg:w-w-14 object-contain"
+                    src={item.img}
+                    alt={item.category}
+                  />
+                  <p className="text-gray-600 text-xs pt-2">{item.category}</p>
+                </div>
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </section>
+    </>
+  );
+};
+
+export default PopularCategories;
